0d3d820b45d65e6a04a9d07edfdc30e8b68cf5f5
haney2121 <haney037332@gmail.com>
2022-08-29 23:11:49 -0500

    added styling and spacing alert manager for items

--- src/components/Alerts/AlertManager.tsx
+++ src/components/Alerts/AlertManager.tsx
@@ -1,17 +1,23 @@
 import Alert from './Alert';
 import { useAlertReducer } from '../../context/AlertContext';
+import { Container, Grid } from '@mui/material';
 
 const AlertManager = () => {
   const { alerts } = useAlertReducer();
 
   return (
-    <div>
+    <Container>
       <h3>Alert Manager</h3>
-      {alerts.map((alert) => (
-        <Alert key={alert.id} {...alert} />
-      ))}
+      <Grid container spacing={2}>
+        {alerts.map((alert) => (
+          <Grid key={alert.id} item xs={12}>
+            <Alert {...alert} />
+          </Grid>
+        ))}
+      </Grid>
+
       {alerts.length === 0 && <p>No Alerts</p>}
-    </div>
+    </Container>
   );
 };
 
